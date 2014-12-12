59c07983702bf911f0bdcf11fec173ec9bdb8df4
Travis Johnson <travis@thisguy.codes>
2014-12-12 00:31:29 -0500

    add indexes to users table

--- db/migrate/20141210053509_create_users.rb
+++ db/migrate/20141210053509_create_users.rb
@@ -1,17 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username,             :null => false 
-      t.string :email,                :null => false 
-      t.string :encrypted_password,   :null => false 
-      t.string :password_salt,        :null => false 
-      t.string :persistence_token,    :null => false 
-
-      # Why not
-      t.string :single_access_token,  :null => false 
-
-      # Useful to impliment resets later
-      t.string :perishable_token,     :null => false 
+      t.string :username,             :null => false
+      t.string :email,                :null => false
+      t.string :encrypted_password,   :null => false
+      t.string :password_salt,        :null => false
+      t.string :persistence_token,    :null => false
 
       # Magic columns, free data!
       t.integer :login_count,         :null => false, :default => 0
@@ -24,5 +18,8 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    add_index :users, :login, :unique => true
+    add_index :users, :persistence_token
   end
 end
