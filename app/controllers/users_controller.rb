84cdc57935fb1b0094a27259932be2d93ac254a7
Travis Johnson <travis@thisguy.codes>
2014-12-12 01:12:09 -0500

    better user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :destroy]
+  respond_to :html, :json
 
   # GET /users/new
   def new
@@ -18,29 +19,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to root_url, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = 'User was successfully created.' if @user.save
+      redirect_to root_url
+    else
+      respond_with @user
     end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to root_url, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update user_params
+      flash[:success] = 'User was successfully updated.'
     end
+    redirect_to edit_user_url
   end
 
   # DELETE /users/1
