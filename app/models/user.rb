0aca1475b560d10b65f2efb7f88288565cff2cc8
Travis Johnson <travis@thisguy.codes>
2014-12-13 00:59:42 -0500

    add posts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
     c.crypto_provider = Authlogic::CryptoProviders::SCrypt
     c.change_single_access_token_with_password = true
   end
+
+  has_many :posts, dependent: :delete_all
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212062428) do
+ActiveRecord::Schema.define(version: 20141213045829) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "sessions", force: true do |t|
     t.string   "session_id", null: false
