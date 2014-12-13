b48d6e839f78fc914d34ab167174d4556f3a442e
Travis Johnson <travis@thisguy.codes>
2014-12-13 00:04:49 -0500

    typo in SCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
-    c.crypto_provider = Authlogic::CryptoProviders::Scrypt
+    c.crypto_provider = Authlogic::CryptoProviders::SCrypt
     c.change_single_access_token_with_password = true
   end
 end
