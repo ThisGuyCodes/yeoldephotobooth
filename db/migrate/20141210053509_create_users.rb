8241b5a95a07e4e2d5de3c44edc24a4fb4187158
Travis Johnson <travis@thisguy.codes>
2014-12-13 23:45:27 -0500

    clean up syntax in migrations

--- db/migrate/20141210053509_create_users.rb
+++ db/migrate/20141210053509_create_users.rb
@@ -1,15 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username,             :null => false
-      t.string :email,                :null => false
-      t.string :encrypted_password,   :null => false
-      t.string :password_salt,        :null => false
-      t.string :persistence_token,    :null => false
+      t.string :username,            null: false
+      t.string :email,               null: false
+      t.string :encrypted_password,  null: false
+      t.string :password_salt,       null: false
+      t.string :persistence_token,   null: false
 
       # Magic columns, free data!
-      t.integer :login_count,         :null => false, :default => 0
-      t.integer :failed_login_count,  :null => false, :default => 0
+      t.integer :login_count,        null: false, default: 0
+      t.integer :failed_login_count, null: false, default: 0
       t.datetime :last_request_at
       t.datetime :current_login_at
       t.datetime :last_login_at
@@ -19,7 +19,7 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :username, :unique => true
+    add_index :users, :username, unique: true
     add_index :users, :persistence_token
     add_index :users, :last_request_at
   end
