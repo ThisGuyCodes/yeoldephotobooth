478d2facf36bd98f7e63bf254e39d474e94436d3
Travis Johnson <travis@thisguy.codes>
2014-12-14 00:27:24 -0500

    require user_id for posts

--- db/migrate/20141213045829_create_posts.rb
+++ db/migrate/20141213045829_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :user, index: true
+      t.references :user, null: false, index: true
 
       t.timestamps
     end
