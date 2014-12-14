47cb39f6d3f39ef006bb35cc9ceb17ae2da3717a
Travis Johnson <travis@thisguy.codes>
2014-12-14 18:16:13 -0500

    working user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,10 +5,22 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
+  test "should get edit" do
+    get :edit, id: @user
     assert_response :success
-    assert_not_nil assigns(:users)
+  end
+
+  test "should update user" do
+    patch :update, id: @user, user: { email: @user.email, password: @user.password, username: @user.username }
+    assert_redirected_to edit_user_url(@user)
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
+    end
+
+    assert_redirected_to root_url
   end
 
   test "should get new" do
@@ -18,32 +30,10 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { email: @user.email, password: @user.password, username: @user.username }
+      post :create, user: { email: "test@email.com", password: "asdf", password_confirmation: "asdf", username: "nottaken" }
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to root_url
   end
 
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch :update, id: @user, user: { email: @user.email, password: @user.password, username: @user.username }
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
-
-    assert_redirected_to users_path
-  end
 end
