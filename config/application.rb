44bd8d7f387ebbf9bc72e6d58a786880547bd234
Travis Johnson <travis@thisguy.codes>
2014-12-14 21:36:22 -0500

    still use bower, but use sass versions of bootstrap

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,15 @@ module Yeoldephotobooth
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Bower asset paths
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.sass.load_paths << bower_path
+      config.assets.paths << bower_path
+    end
+    # Precompile Bootstrap fonts
+    config.assets.precompile << %r(bootstrap-sass-official/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff)$)
+    # Minimum Sass number precision required by bootstrap-sass
+    ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
   end
 end
