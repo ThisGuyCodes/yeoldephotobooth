44dd4ee3f6ccdc1da4891f79829f3013b65a2d7b
Travis Johnson <travis@thisguy.codes>
2014-12-20 05:34:27 -0500

    add image_url to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215035221) do
+ActiveRecord::Schema.define(version: 20141220103141) do
 
   create_table "users", force: true do |t|
     t.string   "username",                       null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141215035221) do
     t.integer  "user_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
     t.index ["created_at"], :name => "index_posts_on_created_at"
     t.index ["user_id"], :name => "index_posts_on_user_id"
     t.foreign_key ["user_id"], "users", ["id"], :on_update => :no_action, :on_delete => :no_action, :name => "fk_posts_user_id"
